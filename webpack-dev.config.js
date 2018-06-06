04e9546bfd6c9cd445e3d3a916151daf73d19bac
Hieu Tran <hieutran@kms-technology.com>
2018-06-06 09:21:19 +0700

    fix webpack config

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -11,7 +11,9 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
-        options: { presets: ["env"], plugins: ["transform-class-properties"] }
+        options: {
+          presets: ["react", "es2015", "stage-1", "env"],
+          plugins: ["transform-decorators-legacy"] }
       },
       {
         test: /\.css$/,
