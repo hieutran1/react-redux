da35dd97fd196f6eb4423199101b3d5715ade0df
hieutran1 <tinhocc23@gmail.com>
2018-07-21 21:02:18 +0700

    react life cycle

---
+++ webpack-dev.react.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const webpack = require("webpack");
+const bundlePath = path.resolve(__dirname, "dist/");
+
+module.exports = {
+  mode: "development",
+  entry: ["babel-polyfill", "./src/React/index.js"],
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["react", "es2015", "stage-1", "env"],
+          plugins: ["transform-decorators-legacy", "transform-class-properties", "transform-regenerator"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|pdf)$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000
+        }
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    publicPath: bundlePath,
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    port: 3001,
+    publicPath: "http://localhost:3001/dist"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
